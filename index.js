c48ea1fe44f7e9f6b99322df309e1246b9428e3d
Dirt Deodara <thecrowleaps@gmail.com>
2019-06-24 17:33:18 -0700

    created my map function

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const map = (arr, callback) => {
+  const mapped = [];
+  for(let i = 0; i < arr.length; i++) {
+    mapped[i] = callback(arr[i]);
+  }
+  return mapped;
+};
+
+module.exports = map;
