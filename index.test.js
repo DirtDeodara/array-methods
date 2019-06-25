a9bfa462b049ab9e7d659b2bb77c8d53f500ac09
Dirt Deodara <thecrowleaps@gmail.com>
2019-06-24 17:36:40 -0700

    refactored tdd for map

--- index.test.js
+++ index.test.js
@@ -3,15 +3,16 @@ const map = require('./index');
 
 
 
-const array = ['hammer', 'nail', 'screwdriver', 'screw'];
+// const array = ['hammer', 'nail', 'screwdriver', 'screw'];
 
 describe('array function tests', () => {
   it('tests map function', () => {
+    const array = ['hammer', 'nail', 'screwdriver', 'screw'];
     const callback = (item) => { 
       return item; 
     };
     const mapper = map(array, callback);
-    expect(mapper).toEqual(['hammer', 'nail', 'screwdriver', 'screw']);
+    expect(mapper).toEqual(array);
 
   });
 
